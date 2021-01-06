2b058c92605adb6cfbf078c8d263488923be0597
SungHyunLim <sunghyun.lim@gmail.com>
2021-01-06 18:09:49 +0900

    django course 25 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -34,3 +34,9 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(DetailView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
